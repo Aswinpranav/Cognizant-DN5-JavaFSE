d3aba665c156bcd693dcb667eb9e41c2895828f9
Aswinpranav <150768346+Aswinpranav@users.noreply.github.com>
2026-06-19 10:11:08 +0530

    Add files via upload

--- Module1-DesignPatterns/SingletonPatternExample/Logger.java
+++ Module1-DesignPatterns/SingletonPatternExample/Logger.java
@@ -1,1 +1,9 @@
+public class Logger {
+    private static Logger instance = new Logger();
+    private Logger(){
 
+    }
+    public static Logger getInstance(){
+        return instance;
+    }
+}
