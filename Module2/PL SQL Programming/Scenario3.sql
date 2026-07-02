d8b669808d86c789358ff7b19c3cc1a983c55190
Aswinpranav <150768346+Aswinpranav@users.noreply.github.com>
2026-07-02 16:54:10 +0530

    Update Scenario3.sql

--- Module2/PL SQL Programming/Scenario3.sql
+++ Module2/PL SQL Programming/Scenario3.sql
@@ -1,1 +1,20 @@
+BEGIN
+    FOR loan_rec IN (
+        SELECT c.Name, l.LoanID, l.DueDate
+        FROM Customers c
+        JOIN Loans l
+        ON c.CustomerID = l.CustomerID
+        WHERE l.DueDate BETWEEN SYSDATE AND SYSDATE + 30
+    )
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(
+            'Reminder: Loan '
+            || loan_rec.LoanID
+            || ' for '
+            || loan_rec.Name
+            || ' is due on '
+            || loan_rec.DueDate
+        );
+    END LOOP;
+END;
 
