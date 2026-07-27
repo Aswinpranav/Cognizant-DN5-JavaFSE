e2d1f86ba07eb285201cfd83847aadea0e9fad07
Aswinpranav <150768346+Aswinpranav@users.noreply.github.com>
2026-07-27 15:10:00 +0530

    Create MVCTest.java

---
+++ Module1-DesignPatterns/ModelViewControllerPattern/MVCTest.java
@@ -0,0 +1,19 @@
+public class MVCTest {
+    public static void main(String[] args) {
+        Student student = new Student("Ezhil", 101, "A");
+        StudentView view = new StudentView();
+        StudentController controller =
+                new StudentController(student, view);
+
+        System.out.println("Initial Student Details:");
+        controller.updateView();
+
+        System.out.println();
+
+        controller.setStudentName("Arun");
+        controller.setStudentGrade("A+");
+
+        System.out.println("Updated Student Details:");
+        controller.updateView();
+    }
+}
