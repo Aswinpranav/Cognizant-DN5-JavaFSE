56c678b14c44802f60129c57f511c1a3b80d9e82
Aswinpranav <150768346+Aswinpranav@users.noreply.github.com>
2026-07-27 15:13:08 +0530

    Create MobileApp.java

---
+++ Module1-DesignPatterns/ObserverPattern/MobileApp.java
@@ -0,0 +1,10 @@
+public class MobileApp implements Observer {
+    private String user;
+    public MobileApp(String user) {
+        this.user = user;
+    }
+    @Override
+    public void update(String stockName, double stockPrice) {
+        System.out.println("Mobile App: [" + user + "] " + stockName + " = Rs. " + stockPrice);
+    }
+}
